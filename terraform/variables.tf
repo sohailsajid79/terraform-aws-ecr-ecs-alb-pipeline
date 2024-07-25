4cf3266e0ced68a08c6be8b786e917b857a7f737
Sohail <sohailsajid79@gmail.com>
2024-07-25 21:52:19 +0000

    rm variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,13 @@
 variable "region" {
   description = "AWS region"
-  default     = "us-east-1"
+  default     = "eu-north-1"
 }
 
 variable "ami" {
   description = "AMI ID"
   default     = "ami-07c8c1b18ca66bb07" # Canonical, Ubuntu, 24.04 LTS
 }
+
 
 variable "instance_type" {
   description = "Instance type"
@@ -15,20 +16,12 @@ variable "instance_type" {
 
 variable "aws_key_pair" {
   description = "Name of the SSH key pair"
-  default = "ssh_key"
+  default     = "ssh_key"
 }
 
-variable "instance_name" {
-  description = "Name of the instance"
-  default     = "flask-app-server"
-}
-
-variable "vpc_id" {
-  description = "VPC ID"
-}
-
-variable "public_key" {
-  description = "Public SSH key"
+variable "public_key_path" {
+  description = "Path to the public SSH key"
+  default     = "my-key-pair.pub"
 }
 
 
