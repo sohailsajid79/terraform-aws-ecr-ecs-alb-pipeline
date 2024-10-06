f5dfa850aba49a1e7dc9c675732e323b66f81aca
SohailSajid <sohailsajid79@gmail.com>
2024-10-06 09:15:45 +0100

    add root terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,25 +3,7 @@ variable "region" {
   default     = "eu-north-1"
 }
 
-variable "ami" {
-  description = "AMI ID"
-  default     = "ami-07c8c1b18ca66bb07" # Canonical, Ubuntu, 24.04 LTS
+variable "repository_name" {
+  type    = string
+  default = "rock-paper-scissors"
 }
-
-
-variable "instance_type" {
-  description = "Instance type"
-  default     = "t3.micro"
-}
-
-variable "aws_key_pair" {
-  description = "Name of the SSH key pair"
-  default     = "ssh_key"
-}
-
-variable "public_key_path" {
-  description = "Path to the public SSH key"
-  default     = "my-key-pair.pub"
-}
-
-
