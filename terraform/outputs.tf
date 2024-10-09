97ea055458636cb69db1dcd22dfab5be2d024e36
SohailSajid <sohailsajid79@gmail.com>
2024-10-09 20:28:22 +0100

    update outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,9 @@
 output "ecr_repository_url" {
-  value = module.ecr.repository_url
+  description = "ECR repository URL"
+  value       = aws_ecr_repository.rock_paper_scissors.repository_url
+}
+
+output "alb_dns_name" {
+  value       = aws_lb.rock_paper_scissors_alb.dns_name
+  description = "The DNS name of the ALB to access the application"
 }
