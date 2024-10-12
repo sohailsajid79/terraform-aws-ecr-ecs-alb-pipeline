c588727795fca028fa009b60531cbe7d787c740b
SohailSajid <sohailsajid79@gmail.com>
2024-10-12 11:11:19 +0100

    update

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -8,6 +8,14 @@ resource "aws_security_group" "alb_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
