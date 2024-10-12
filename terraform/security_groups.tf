49c4ca84474d2615b8a5f4d5ef7aa11b9f2f7638
SohailSajid <sohailsajid79@gmail.com>
2024-10-12 02:28:07 +0100

    update

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -1,3 +1,25 @@
+resource "aws_security_group" "alb_sg" {
+  name   = "rock-paper-scissors-alb-sg"
+  vpc_id = aws_vpc.ecs_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-security-group"
+  }
+}
+
 resource "aws_security_group" "ecs_service_sg" {
   name   = "rock-paper-scissors-ecs-sg"
   vpc_id = aws_vpc.ecs_vpc.id
@@ -18,35 +40,5 @@ resource "aws_security_group" "ecs_service_sg" {
   }
   tags = {
     Name = "ecs-security-group"
-  }
-}
-
-resource "aws_security_group" "alb_sg" {
-  name   = "rock-paper-scissors-alb-sg"
-  vpc_id = aws_vpc.ecs_vpc.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "alb-security-group"
   }
 }
